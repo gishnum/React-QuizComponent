704b7094a5dfa562e6c3e87bf6cb156b4a8269e0
gishnum <42067062+gishnum@users.noreply.github.com>
2021-09-30 19:30:59 +0530

    changes app js

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,9 +9,12 @@ class Quiz extends Component {
     }
     render () {
       return (
-        <div>Quiz</div>
+        <div>
+            <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+            </div>
       )
     }
   }
   
   export default Quiz
+  
